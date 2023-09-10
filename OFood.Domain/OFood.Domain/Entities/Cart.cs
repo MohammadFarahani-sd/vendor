3d300bb9e1ea8b7eccc5fc7bb6f0cb07be206e2e
JavidPour.Mohammad <JavidPour.Mohammad@TEK001N4446>
2023-09-10 17:47:17 +0330

    Push

---
+++ OFood.Domain/OFood.Domain/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿using OFood.Domain.Commons;
+
+namespace OFood.Domain.Entities;
+
+public class Cart : Entity<long>
+{
+    public int CartCount { get; set; }
+
+    public long CartIddsds { get; set;}
+}
